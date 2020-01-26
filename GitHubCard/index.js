9edcf5fd61fbeb0087925a2787327baca629cc31
Khoa Tran <jacktran1799@gmaail.com>
2020-01-26 14:45:03 -0600

    finished

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,12 +1,39 @@
 /* Step 1: using axios, send a GET request to the following URL 
-           (replacing the palceholder with your Github name):
+           (replacing the placeholder with your Github name):
            https://api.github.com/users/<your name>
 */
+axios
+.get('https://api.github.com/users/Jacktran123')
+.then( response => {
+  console.log(response);
+  let cardcomponent2=cardcomponent(response.data);
+  parentDivCard.appendChild(cardcomponent2);
+});
+
+const friendsArray = [
+  'tetondan',
+  'dustinmyers',
+  'justsml',
+  'luishrd',
+  'bigknell' ];
+
+friendsArray.forEach(user => {
+  axios
+    .get(`https://api.github.com/users/${user}`)
+    .then(response => {
+      console.log(response);
+      let cardcomponent3=cardcomponent(response.data);
+      parentDivCard.appendChild(cardcomponent3);
+    });
+    
+    
+});
+
 
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
    data in order to use it to build your component function 
-
+ 
    Skip to Step 3.
 */
 
@@ -24,12 +51,67 @@
           user, and adding that card to the DOM.
 */
 
-const followersArray = [];
 
+    
 /* Step 3: Create a function that accepts a single object as its only argument,
-          Using DOM methods and properties, create a component that will return the following DOM element:
+          Using DOM methods and properties, create a component that will return the following DOM element:*/
+const parentDivCard=document.querySelector('.cards');
 
-<div class="card">
+ function cardcomponent(obj){
+  let divcard=document.createElement('div');
+  divcard.classList.add('card');
+
+  let imgcard=document.createElement('img');
+  imgcard.src= obj.avatar_url;
+
+  let divinfocard=document.createElement('div');
+  divinfocard.classList.add('card-info');
+
+  let h3card=document.createElement('h3');
+  h3card.classList.add('name')
+  h3card.textContent= `${obj.name}`;
+
+
+  let pcard1=document.createElement('p');
+  pcard1.classList.add('username');
+  pcard1.textContent= `${obj.login}`;
+
+  let plocation=document.createElement('p')
+  plocation.textContent=`${obj.location}`;
+
+  let pfile=document.createElement('p');
+  pfile.textContent= `Profile :`;
+  
+  let afile=document.createElement('a');
+  afile.href= `${obj.html_url}`;
+  afile.textContent=  `${obj.html_url}`;
+
+  let pcard2=document.createElement('p');
+  pcard2.textContent= `Followers : ${obj.followers}`;
+
+  let pcard3=document.createElement('p');
+  pcard3.textContent= `Following : ${obj.following}`;
+
+  let pcard4= document.createElement('p')
+  pcard4.textContent= ` Bio : ${obj.bio}`;
+
+  divcard.appendChild(imgcard);
+  divcard.appendChild(divinfocard);
+  divinfocard.appendChild(h3card);
+  divinfocard.appendChild(pcard1);
+  divinfocard.appendChild(plocation);
+  divinfocard.appendChild(pfile);
+  pfile.appendChild(afile);
+  divinfocard.appendChild(pcard2);
+  divinfocard.appendChild(pcard3);
+  divinfocard.appendChild(pcard4);
+  
+
+  return divcard;
+
+}
+      
+/* <div class="card">
   <img src={image url of user} />
   <div class="card-info">
     <h3 class="name">{users name}</h3>
